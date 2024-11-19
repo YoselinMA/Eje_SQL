2c601a0c1a5a04f47b2b0f1dbd82c862b9dc1cee
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-11-18 22:47:05 -0600

    Update Eje1.sql

--- Eje1.sql
+++ Eje1.sql
@@ -1,1 +1,13 @@
+-- Crear una tabla llamada Empleados, tendra como atributos numero de empleado, nombre del empleado, apellido 1 del empleado, apellido 2, email, salario que sea mayor a 3000 pero menor que 5000
+-- y checar que el correo electronico sea veridico
 
+CREATE TABLE Empleados (
+    no_empleado INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(30) NOT NULL,
+    Apellido1 VARCHAR(30) NOT NULL,
+    Apellido2 VARCHAR(30) NOT NULL,
+    email VARCHAR(30) UNIQUE NOT NULL,
+    Salario INT,
+    CHECK (Salario >= 3000 AND Salario <= 50000),
+    CHECK (email LIKE '%_@__%.__%')
+);
