ef588c03380281081216b99a29027d3537b0d801
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-11-19 22:28:38 -0600

    Update Eje3.sql

--- Eje3.sql
+++ Eje3.sql
@@ -1,1 +1,9 @@
+-- Crea una tabla producto 
 
+CREATE TABLE Productos (
+    nombre_producto INT PRIMARY KEY AUTO_INCREMENT,
+    codigo_barras VARCHAR(100) UNIQUE NOT NULL,
+    precio DECIMAL(10, 2) 
+    CHECK (precio > 0),
+    stock INT DEFAULT 100 NOT NULL
+);
