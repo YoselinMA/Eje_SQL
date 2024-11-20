de5f5c23900adc9972a9b1878c9a14422e36aa7b
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-11-19 22:29:37 -0600

    Update Eje4.sql

--- Eje4.sql
+++ Eje4.sql
@@ -1,1 +1,10 @@
+-- Crea ablla Pedido
+
+CREATE TABLE Pedidos (
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+		cantidad INT NOT NULL,
+    CHECK (cantidad >= 1),
+    total INT NOT NULL,
+    CHECK (total = cantidad * 10)
+);
 
